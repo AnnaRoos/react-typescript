a675099ddd20a8e75afe260af38489442c4defa0
Anna Roos <anna.flytstrom@gmail.com>
2022-02-28 20:07:10 +0100

    added function as prop, added styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 
+import NewTodo from './components/NewTodo';
 import Todo from './models/todo';
 import Todos from './components/Todos';
 
 function App() {
-  const todos = [new Todo('Learn stuff'), new Todo('Clean')];
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const addTodoHandler = (text: string) => {
+    const newTodo = new Todo(text);
+    setTodos((prev) => prev.concat(newTodo));
+  };
+
   return (
     <div>
+      <NewTodo onAddTodo={addTodoHandler} />
       <Todos items={todos} />
     </div>
   );
--- src/components/NewTodo.tsx
+++ src/components/NewTodo.tsx
@@ -1,6 +1,8 @@
 import React, { useRef } from 'react';
 
-const NewTodo = () => {
+import classes from './NewTodo.module.css';
+
+const NewTodo: React.FC<{ onAddTodo: (text: string) => void }> = (props) => {
   const inputRef = useRef<HTMLInputElement>(null);
 
   const submitHandler = (event: React.FormEvent) => {
@@ -9,10 +11,11 @@ const NewTodo = () => {
     if (enteredText.trim().length === 0) {
       return;
     }
+    props.onAddTodo(enteredText);
   };
 
   return (
-    <form onSubmit={submitHandler}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <label htmlFor="text">Todo</label>
       <input type="text" id="text" ref={inputRef} />
       <button type="submit">Add Todo</button>
